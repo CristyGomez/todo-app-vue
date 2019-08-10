dbc2d6fda622eae9d92ba36685cbfadccf31e42c
Gabriel <gaps18072001@gmail.com>
2019-08-09 22:53:06 -0400

    Se colocó una img de basurita y se encogió la letra

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,19 +2,19 @@
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-bold font-hairline text-6xl text-center text-orange-400">PATITOH</h1>
 
-        <todo-card class="mx-auto mt-4 h-auto w-full max-w-lg">
+        <todo-card class="mx-auto mt-4 h-auto w-full max-w-sm">
         <div>
             <input placeholder="New Task" @keyup.enter="addTodo" v-model="newTodo" class="bg-gray-100 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-900 leading-tight focus:outline-none focus:bg-white focus:border-yellow-500">
             <hr>
             <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
-                <div class="todo-item-left text-xl">
+                <div class="todo-item-left text-base">
                     <input class="checkbox" type="checkbox" v-model="todo.completed">
                    <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed}">{{todo.title}}</div>
                    <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
                 </div>
-                <div class="remove-item cursor-pointer" @click="removeTodo(index)">
-                    &times;
-                </div>
+                <img src="https://github.com/GabrielAps18/pruebas/blob/gabrielaps/css/recycle%20(1).png?raw=true" class="remove-item cursor-pointer" @click="removeTodo(index)">
+                   
+                </img>
             </div>
         </div>
         </todo-card>
